a10e53c4a5f39489487727b9bf4f080e088d1cdc
simonsben <simons.ben0@gmail.com>
2018-11-19 21:04:37 -0500

    k-means doesn't work for us (I think), moving to DBSCAN

--- utilities/plots.py
+++ utilities/plots.py
@@ -1,8 +1,10 @@
-from matplotlib.pyplot import figure, imshow, show
+from matplotlib.pyplot import figure, imshow, show, title
 
 
 # Plots image (mainly for debugging
-def display_image(image, blocking=False):
-    figure('Image')                 # Create new figure
+def display_image(image, _title='Test image', blocking=False):
+    figure(_title)                 # Create new figure
     imshow(image, cmap='gray')      # Display image (as greyscale
+    if _title != 'Test image':
+        title(_title)
     show(block=blocking)            # Show figure/make blocking or not
